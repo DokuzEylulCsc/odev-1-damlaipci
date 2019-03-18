e42e1cb377728202e0cd81a3eeb04d45ec9aaef6
damlaipci <47853227+damlaipci@users.noreply.github.com>
2019-03-18 20:50:19 +0300

    Add files via upload

---
+++ Odev_1/ErMeydani.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ODEV1
+{
+    class ErMeydani
+    {
+        Bolge[,] harita = new Bolge[16, 16];
+
+        public Bolge[,] Harita { get { return harita; } set { harita = value; } }
+
+        public ErMeydani(Takim t)
+        {
+            for (int i = 0; i < 16; i++)
+            {
+                for (int j = 0; j < 16; j++)
+                {
+                    Harita[i, j] = new Bolge(i, j);
+                }
+            }
+            
+        }
+
+    }
+}
--- Odev_1/Yuzbasi.cs
+++ Odev_1/Yuzbasi.cs
@@ -2,11 +2,90 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
-namespace Odev_1
+namespace ODEV1
 {
-    class Yuzbasi : Asker
+    internal class Yuzbasi : Asker
     {
-        // ..... //
+
+        public Yuzbasi(int takim, int x, int y)
+        {
+            takimBilgisi = takim;
+            Xkonum = x;
+            Ykonum = y;
+        }
+        public override void Bekle()
+        {
+            //Bırsey yapmıyor.
+        }
+        public override void Hareket_Et(Object[,] saha)
+        {
+            Random random = new Random();
+            bool kontrol, kontrol2;
+            int xSans, ySans, atesEtme;
+            do
+            {
+                do
+                {
+                    kontrol = true;
+                    xSans = random.Next(-1, 2);
+                    ySans = random.Next(-1, 2);
+                    if (xSans == 0 && ySans == 0)
+                        kontrol = false;
+                } while (!kontrol);
+
+                if (saha[Xkonum + xSans, Ykonum + ySans] is Bolge b)
+                {
+                    Xkonum = +xSans;
+                    Ykonum = +ySans;
+                    kontrol2 = true;
+                }
+                else
+                {
+                    Bekle();
+                    kontrol2 = true;
+                }
+            } while (!kontrol2);
+            atesEtme = random.Next(0, 2);
+            if (atesEtme == 1)
+            {
+                Ates_Et(saha);
+            }
+        }
+        public override void Ates_Et(Object[,] saha)
+        {
+            Random random = new Random();
+            int sans = random.Next(100);
+            for (int i = -3; i < 4; i++)       //komşusu olan tüm askerlere vuruyo
+                for (int j = -3; j < 4; j++)
+                {
+                    if (saha[i, j] is Asker a && a.takimBilgisi != this.takimBilgisi)
+                    {
+                        if (sans < 10)       //10
+                        {
+                            a.can = -40;
+                            Yazdirma.Yazdır("Cani 10 azaldı");
+                        }
+                        else if (sans < 40)     //30
+                        {
+                            a.can = -25;
+                            Yazdirma.Yazdır("Canı 30 azaldı");
+                        }
+                        else    //60
+                        {
+                            a.can = -15;
+                            Yazdirma.Yazdır("Canı 60 azaldı");
+                        }
+                        if (a.can <= 0)
+                        {
+                            a.yasam = false;
+                            Yazdirma.Yazdır("Öldü");
+                            saha[Xkonum, Ykonum + 2] = new Bolge(Xkonum, Ykonum + 2);
+                        }
+                    }
+                }
+        }
+
     }
 }
